dacbd06bdebab03845c77473bbac122a0bea0f27
Poylar <doaleks8@gmail.com>
2024-02-08 14:42:08 +0800

    partners sections

--- src/components/styles/Description.styled.ts
+++ src/components/styles/Description.styled.ts
@@ -1,13 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const Description = styled.p`
-  font-size: 0.9375rem;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  text-transform: uppercase;
-
-  @media screen and (min-width: 764px) {
-    font-size: 2.5rem;
-  }
-`;
+export const Description = styled.p<{
+	$px?: string
+	$pxMobile?: string
+	$fontSizeDesktop?: string
+	$fontSizeMobile?: string
+}>`
+	font-size: ${props => props.$fontSizeMobile || '0.9375rem'};
+	font-style: normal;
+	font-weight: 500;
+	line-height: normal;
+	text-transform: uppercase;
+	padding-inline: ${props => props.$pxMobile};
+	@media screen and (min-width: 764px) {
+		padding-inline: ${props => props.$px};
+		font-size: ${props => props.$fontSizeDesktop || '2.5rem'};
+	}
+`
